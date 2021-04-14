c9174d6378008e981349d744826ad44c7f35388f
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-04-14 09:16:51 +0200

    la till några saker till jobbet jag har gjort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     VarderingarComponent,
     InriktningarComponent,
     OmOssComponent,
-    KingpinKidsComponent
+    KingpinKidsComponent,
     NavbarComponent
   ],
   imports: [
