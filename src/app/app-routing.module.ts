2e14d99bf4a0b650bab4f4cc43b8c30bca4098e5
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-04-14 14:48:15 +0200

    nothing importent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import {NavbarComponent} from './navbar/navbar.component'
 import {OmOssComponent} from './om-oss/om-oss.component'
+
+
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+
 const routes: Routes = [ 
   { path: '', component: HomeComponent  },
   { path: 'about', component: OmOssComponent },
-  { path: 'contact', component: NavbarComponent },
   {path: 'home', component: HomeComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'faq', component: FaqComponent}
