f8739ad32aeddd1dbd44ac5874a737e775585102
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-04-14 14:37:21 +0200

    fixed some buggs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {NavbarComponent} from './navbar/navbar.component'
-
+import {OmOssComponent} from './om-oss/om-oss.component'
 const routes: Routes = [ 
   { path: '', component: NavbarComponent  },
-  { path: 'about', component: NavbarComponent },
+  { path: 'about', component: OmOssComponent },
   { path: 'contact', component: NavbarComponent },
 ];
 
