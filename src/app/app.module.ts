4cb87e2ebcacbf6b63ac8a52587808fefd167443
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-04-21 08:25:14 +0200

    done with the calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgmCoreModule } from '@agm/core'
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +22,7 @@ import { MedlemskapComponent } from './medlemskap/medlemskap.component';
 
 @NgModule({
   declarations: [
+    
     AppComponent,
     VarderingarComponent,
     InriktningarComponent,
@@ -36,6 +37,8 @@ import { MedlemskapComponent } from './medlemskap/medlemskap.component';
     MedlemskapComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
--- src/app/medlemskap/medlemskap.component.ts
+++ src/app/medlemskap/medlemskap.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-medlemskap',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medlemskap.component.css']
 })
 export class MedlemskapComponent implements OnInit {
-
   constructor() { }
+  
 
+  total: number = 0
+
+  form = new FormGroup({
+    choice: new FormControl(''),
+    student:new FormControl(false),
+    membership: new FormControl(false)
+  });
+
+  valueChanged(){
+    const formValue = this.form.value;
+
+    this.total = formValue.choice as number;
+    this.total -= 0;
+
+
+    if(formValue.student && (formValue.choice == 1600 || formValue.choice == 2000 )){
+      this.total  -= 400 ;
+    }
+    if(formValue.membership){
+      let holder:number = this.total;
+      holder = holder + 100 ;
+      this.total = holder as number;
+    }
+    console.log(this.total);
+  }
   ngOnInit(): void {
   }
-
 }
