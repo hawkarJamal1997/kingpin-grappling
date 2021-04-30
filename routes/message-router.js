8e8197777332eb257e8a4925755583be09ab2e64
Hawkar jamal <48949166+hawkarJamal1997@users.noreply.github.com>
2021-04-30 15:10:23 +0200

    fixed functionality for sending mail

--- routes/message-router.js
+++ routes/message-router.js
@@ -1,11 +1,31 @@
 const express = require('express')
-const Schedule = require('../models/Schedule')
+const fetch = require('node-fetch')
+const nodemailer = require('nodemailer')
 
 const router = express.Router()
 
-//Get message
-router.get('/', async (req, res) => {
-    res.
+router.post('/', async (req, res) => {
+
+  // Ett fake konto där man kan se meddelanden i ethereal.mail med dessa uppgifter
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'catherine.heathcote66@ethereal.email',
+        pass: 'UjU6H6ZnY6ZhF2C7Ny'
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `${req.body.name} " " ${req.body.email}`,
+    to: "catherine.heathcote66@ethereal.email", // list of recievers
+    subject: "Contact kingping", 
+    text: `${req.body.message}`, 
+    html: `<b>${req.body.message}</b>`, 
+  });
+
+  res.status(200).end()
 })
 
 module.exports = router
